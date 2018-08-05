025c1c061861aa088b826264287767c438d91511
chandramohan <chandramohan@DESKTOP-LJSGCTG>
2018-08-04 22:16:10 -0700

    Permutation chages

---
+++ C# Programes Basic/ProblemSolving/LogicalProblems/LogicalProblems/TreeDisplay.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    public class TreeDisplay
+    {
+        public int VLevel { get; set; }
+
+        public int HLevel { get; set; }
+
+        public static void Display(List<TreeDisplay> treeNodes)
+        {
+            var temp = treeNodes.GroupBy(x => x.VLevel).OrderBy(x => x.Key);
+
+            foreach (var item in temp)
+            {
+                Console.WriteLine(item.Key);
+                Console.Write(string.Join("\t", treeNodes.Where(x => x.VLevel == item.Key).Distinct().Select(x => x.HLevel).Distinct()));
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C# Programes Basic/ProblemSolving/LogicalProblems/LogicalProblems/permutations.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    public static class permutations
+    {
+        private static List<TreeDisplay> treeDisplay = new List<TreeDisplay>();
+        private static void permute(String str, int l, int r)
+        {
+
+            if (l == r)
+            {
+                ////Console.WriteLine(str);
+            }
+            else
+            {
+                for (int i = l; i <= r; i++)
+                {
+                    treeDisplay.Add(new TreeDisplay() { HLevel = i, VLevel = l });
+                    str = swap(str, l, i);
+                    permute(str, l + 1, r);
+                    str = swap(str, l, i);
+                }
+            }
+        }
+
+        public static String swap(String a,
+                              int i, int j)
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+
+        public static void Main(String str)
+        {
+            int n = str.Length;
+            permute(str, 0, n - 1);
+            TreeDisplay.Display(treeDisplay);
+            Console.ReadLine();
+        }
+    }
+}
